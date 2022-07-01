1bed2e24bb159fa0bc0d67fed9ebc004b6a2bab7
Your Name <you@example.com>
2022-07-01 19:08:18 +0530

    sum of command-line arguments and count the invalid integers entered

--- src/day1/Day1_practice.java
+++ src/day1/Day1_practice.java
@@ -1,20 +1,22 @@
 package day1;
 
-import java.util.Scanner;
 
 public class Day1_practice {
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter the two strings to compare");
-		String str1= sc.nextLine();
-		String str2= sc.nextLine();
-		if(str1.equals(str2)) {
-			System.out.println("equal");
-		}
-		else {
-			System.out.println("Not equal");
-		}
+
+	public static void main(String args[]) {
+		int sum = 0;
+		int inv = 0;
+		for (int i = 0; i < args.length; i++) {
+			try {
+				sum += Integer.parseInt(args[i]);
+			} catch (NumberFormatException e) {
+				inv++;
+			}
+			}
+		System.out.println("Total no.of arguments: "+args.length);
+		System.out.println("Invalid Integers: "+inv);
+		System.out.println("Sum: "+sum);
+		
 	}
 
 }
