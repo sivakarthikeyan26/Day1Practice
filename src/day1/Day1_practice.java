648f235843518c57f0bc478620e9d9143e6e0a0a
Your Name <you@example.com>
2022-07-01 19:13:21 +0530

    name using the command line

--- src/day1/Day1_practice.java
+++ src/day1/Day1_practice.java
@@ -4,19 +4,10 @@ package day1;
 public class Day1_practice {
 
 	public static void main(String args[]) {
-		int sum = 0;
-		int inv = 0;
 		for (int i = 0; i < args.length; i++) {
-			try {
-				sum += Integer.parseInt(args[i]);
-			} catch (NumberFormatException e) {
-				inv++;
-			}
-			}
-		System.out.println("Total no.of arguments: "+args.length);
-		System.out.println("Invalid Integers: "+inv);
-		System.out.println("Sum: "+sum);
+			System.out.println(args[i]);
 		
 	}
 
+}
 }
