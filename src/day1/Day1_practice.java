25e868118dfd8ebc150b543f6d30d619ed773ca1
Your Name <you@example.com>
2022-07-01 18:35:03 +0530

    primitive data types

--- src/day1/Day1_practice.java
+++ src/day1/Day1_practice.java
@@ -1,9 +1,19 @@
 package day1;
 
 public class Day1_practice {
-
+	static boolean val1;
+	   static double val2;
+	   static float val3;
+	   static int val4;
+	   static long val5;
+	   static String val6;
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println(val1);
+	    System.out.println(val2);
+	    System.out.println(val3);
+	    System.out.println(val4);
+	    System.out.println(val5);
+	    System.out.println(val6);
 
 	}
 
